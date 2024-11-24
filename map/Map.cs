4bb502245cd641cb45beeddcb2be521b9d9fe952
Alexandre Zenon <ahzed11@protonmail.com>
2024-11-24 14:49:37 +0100

    Disable some nodes in the graph if they are above acceptable cost

--- map/Map.cs
+++ map/Map.cs
@@ -4,22 +4,57 @@ using System;
 
 public partial class Map : TileMapLayer
 {
-	AStar2D astar;
+    AStar2D astar;
+    Dictionary<Vector2I, int> positionToIndex;
+    Array<Vector2I> usedCells;
+
+    private void EnableNavPointsUpTo(int x)
+    {
+        foreach (var cell in usedCells)
+        {
+            var cellData = GetCellTileData(cell);
+            int cost = (int)cellData.GetCustomData("cost");
+            var isDisabled = false;
+            if (cost > x)
+            {
+                isDisabled = true;
+            }
+            var pointId = positionToIndex[cell];
+            astar.SetPointDisabled(pointId, isDisabled);
+        }
+    }
+
+    private void Debug()
+    {
+        EnableNavPointsUpTo(1);
+
+        foreach (var cell in usedCells)
+        {
+            string isEnabled = astar.IsPointDisabled(positionToIndex[cell]) ? "Disabled" : "Enabled";
+            var label = new Label
+            {
+                Text = $"{isEnabled} ({positionToIndex[cell]})",  // Use coordinates or tileId, as needed
+                Position = MapToLocal(cell),  // Offset the position slightly
+            };
+            AddChild(label);
+        }
+    }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-	{
-		astar = new();
-		var usedCells = GetUsedCells();
+    {
+        astar = new();
+        positionToIndex = new();
+        usedCells = GetUsedCells();
+
         int index = 1;
-        Dictionary<Vector2I, int> positionToIndex = new();
 
         foreach (var cell in usedCells)
-		{
+        {
             astar.AddPoint(index, cell);
-			positionToIndex.Add(cell, index);
-			index++;
-		}
+            positionToIndex.Add(cell, index);
+            index++;
+        }
 
         foreach (var cell in usedCells)
         {
@@ -35,20 +70,10 @@ public partial class Map : TileMapLayer
                 astar.ConnectPoints(cellIndex, neighborIndex, true);
             }
         }
-
-		foreach (var cell in usedCells)
-		{
-            var label = new Label
-            {
-                Text = $"({positionToIndex[cell]})",  // Use coordinates or tileId, as needed
-                Position = MapToLocal(cell),  // Offset the position slightly
-            };
-            AddChild(label);
-        }
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
